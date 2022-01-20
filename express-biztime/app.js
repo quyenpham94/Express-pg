bcc6a9be1c1fb2ce3ce161ac668ba37194d69990
Quyen Pham <hoangquyen174@gmail.com>
2022-01-19 23:14:54 -0600

    upload invoices route

--- express-biztime/app.js
+++ express-biztime/app.js
@@ -5,14 +5,14 @@ const express = require("express");
 
 const ExpressError = require("./expressError")
 const companiesRoutes = require("./routes/companies");
-// const invoicesRoutes = require("./routes/invoices");
+const invoicesRoutes = require("./routes/invoices");
 
 const app = express();
 
 
 app.use(express.json());
 app.use("/companies", companiesRoutes)
-// app.use("/invoices", invoicesRoutes)
+app.use("/invoices", invoicesRoutes)
 
 /** 404 handler */
 
--- express-biztime/routes/companies.js
+++ express-biztime/routes/companies.js
@@ -61,7 +61,7 @@ router.put("/:code", async function (req, res, next) {
         const result = await db.query(`UPDATE companies SET name=$1, description=$2 WHERE code = $3 RETURNING code, name, description`,
         [name, description, code]);
 
-        if (result.rows.length == 0) {
+        if (result.rows.length === 0) {
             throw new ExpressError(`No such company: ${code}`, 404)
         } else {
             return res.json({"company": result.rows[0]});
@@ -76,7 +76,7 @@ router.delete("/:code", async function(req, res, next) {
         let code = req.params.code;
         const result = await db.query(`DELETE FROM companies WHERE code=$1 RETURNING code`, [code]);
 
-        if (result.rows.length == 0) {
+        if (result.rows.length === 0) {
             throw new ExpressError(`No such company: ${code}`, 404)
         } else {
             return res.json({"status": "deleted"});
